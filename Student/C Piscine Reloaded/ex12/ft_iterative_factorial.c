57cd76145c46815869c720e178d5ed0cd5517750
pepinillo42 <cynthiabarleycorn@gmail.com>
2024-06-19 23:15:18 +0200

    Commit ex12

---
+++ Student/C Piscine Reloaded/ex12/ft_iterative_factorial.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cybarley <cybarley@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/18 20:00:56 by cybarley          #+#    #+#             */
+/*   Updated: 2024/06/19 23:12:53 by cybarley         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int ft_iterative_factorial(int nb)
+{
+    int i;
+    int factorial;
+
+    if (nb < 0)
+        return 0;
+    
+    factorial = 1; 
+
+        for(i = 1; i <= nb; i++)
+        {
+        factorial *= i;  
+        }
+
+        return factorial;
+}
+
+int main(void)
+{
+    int result = ft_iterative_factorial(4);
+    return result;
+}
