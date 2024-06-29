d82ac7012c1dcc37ce1d06a7284352f3db539c6e
pepinillo42 <cynthiabarleycorn@gmail.com>
2024-06-29 20:05:57 +0200

    ex17

--- Student/C Piscine Reloaded/ex17/ft_strcmp.c
+++ Student/C Piscine Reloaded/ex17/ft_strcmp.c
@@ -1,28 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cybarley <cybarley@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/28 11:06:54 by cybarley          #+#    #+#             */
-/*   Updated: 2024/06/28 15:06:51 by cybarley         ###   ########.fr       */
+/*   Created: 2024/06/29 19:26:38 by cybarley          #+#    #+#             */
+/*   Updated: 2024/06/29 20:04:04 by cybarley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_putstr(char *str)
+int ft_strcmp(char *s1, char *s2)
 {
-    while(*str != '\0')
+    int pos;
+
+    pos = 0;
+
+    while(s1[pos] == s2[pos] && s1[pos] != '\0' && s2[pos] !='\0')
     {
-        write(1, str, 1);
-        str++;
+        pos++;
     }
+    return (s1[pos] - s2[pos]);
 }
 
 /*int main(void)
 {
-    ft_putstr("Jelou");
-    return (0);
+    char *str1 = "Hello";
+    char *str2 = "Hello";
+    int result = ft_strcmp(str1, str2);
+    return (result);
 }*/
